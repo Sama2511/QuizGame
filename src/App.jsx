1969fe41980f5785feafc285fecaa7a0b0dee8d4
Oussama <Hassouneoussama8@gmail.com>
2025-07-09 23:13:54 +1000

    done with rendering questions and answers using trivia API

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ function App() {
 // Switch Page
 const [ShowPage1, setShowPage1] = useState(true)
 const [ShowPage2, setShowPage2] = useState(false)
-const [dataFectched, setData] = useState([])
-// const [Questions , setQuestions]= useState(null)
-let renderQuestions =[]
 
 function Switch(){
   setShowPage1(false)
@@ -17,43 +14,12 @@ function Switch(){
 }
 ////////
 
-useEffect(() => {
-  async function fetchQuestions() {
-    try {
-      const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
-      const data = await res.json()
-      setData(data)
-      console.log('data fetched')
-    } catch (err) {
-      console.error("Fetch error:", err)
-    }
-  }
-  fetchQuestions()
-}, [])
-
-
-if(dataFectched.length ===0){
-  console.log('0')
-}else{
-        renderQuestions = dataFectched.results.map((result,index)=>{
-          console.log(result)
-        return(
-        <SecondPage 
-              question= {result.question} 
-              answer1={result.correct_answer}
-              answer2={result.correct_answer}
-              answer3={result.correct_answer}
-              answer4={result.correct_answer}
-            />
-        )
-      })
-    }
 
   return (
     
     <>
       {ShowPage1 && <FirstPage switch= {Switch}/>}
-      {ShowPage2 && renderQuestions}
+      {ShowPage2 && <SecondPage/>}
     </>
   )
 }
--- src/SecondPage.jsx
+++ src/SecondPage.jsx
@@ -1,17 +1,53 @@
+import { useEffect, useState } from "react"
+import {decode} from 'html-entities';
 
-export default function SecondPage(props){
+export default function SecondPage(){
+
+    const [dataFectched, setData] = useState([])
+
+
+
+
+    useEffect(() => {
+    async function fetchQuestions() {
+        try {
+        const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
+        const data = await res.json()
+        setData(data)
+        console.log('data fetched')
+        } catch (err) {
+        console.error("Fetch error:", err)
+        }
+    }
+    fetchQuestions()
+    }, [])
+
+    let renderQuestions = []
+    if(dataFectched.length ===0){
+    console.log('0')
+    }else{
+    renderQuestions = dataFectched.results.map((result,index)=>{
+        result.question = decode(result.question) 
+        result.allAnswers = [result.correct_answer, ...result.incorrect_answers]
+        result.allAnswers = result.allAnswers.map(answer=> decode(answer))
+        return(
+            <div className="quiz-container" key={index}>
+                    <h1>{result.question}</h1>
+                    <div className="buttons-container">
+                        <button>{result.allAnswers[0]}</button>
+                        <button>{result.allAnswers[1]}</button>
+                        <button>{result.allAnswers[2]}</button>
+                        <button>{result.allAnswers[3]}</button>
+                    </div>
+            </div>
+            )
+        })
+    }
+
     return(
         <>
             <main className="page2-container">
-                <div className="quiz-container">
-                    <h1>{props.question}</h1>
-                    <div className="buttons-container">
-                        <button>{props.answer1}</button>
-                        <button>{props.answer2}</button>
-                        <button>{props.answer3}</button>
-                        <button>{props.answer4}</button>
-                    </div>
-                </div>
+                {renderQuestions}
                 <button id="check-btn">Check answers</button>
             </main>
         </>
