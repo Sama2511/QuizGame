63ee704f17a1ff13cf99f5bfdf7105936dabedb8
Oussama <Hassouneoussama8@gmail.com>
2025-07-10 16:10:58 +1000

    Done with page two logic, just starting on page three

--- src/SecondPage.jsx
+++ src/SecondPage.jsx
@@ -4,8 +4,7 @@ import {decode} from 'html-entities';
 export default function SecondPage(){
 
     const [dataFectched, setData] = useState([])
-
-
+    const [choosenAnswers, setChoosenAnswers] = useState({})
 
 
     useEffect(() => {
@@ -13,7 +12,17 @@ export default function SecondPage(){
         try {
         const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
         const data = await res.json()
-        setData(data)
+        const shuffledAnswers = data.results.map((answer)=>{
+            const allAnswers = [answer.correct_answer, ...answer.incorrect_answers ]
+            shuffleArray(allAnswers)
+            return {
+                'question': answer.question,
+                'correctAnswers': answer.correct_answer,
+                'incorrectAnswers' : answer.incorrect_answers,
+                'allAnswers' : allAnswers
+            }
+        })
+        setData(shuffledAnswers)
         console.log('data fetched')
         } catch (err) {
         console.error("Fetch error:", err)
@@ -22,28 +31,59 @@ export default function SecondPage(){
     fetchQuestions()
     }, [])
 
+    function handleAnswerClick(index,answer){
+        setChoosenAnswers(prev => ({
+            ...prev , [index] : answer 
+        })
+        )
+    }
+    console.log(choosenAnswers)
+
+
+    function shuffleArray(array){
+    let currentIndex = array.length
+
+    while(currentIndex != 0){
+        let randomIndex = Math.floor(Math.random()*currentIndex)
+        currentIndex --
+    
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    }
+
+
+    
     let renderQuestions = []
     if(dataFectched.length ===0){
     console.log('0')
     }else{
-    renderQuestions = dataFectched.results.map((result,index)=>{
+    renderQuestions = dataFectched.map((result,index)=>{
         result.question = decode(result.question) 
-        result.allAnswers = [result.correct_answer, ...result.incorrect_answers]
         result.allAnswers = result.allAnswers.map(answer=> decode(answer))
+        const availableAnswers = result.allAnswers
+
+        const buttonRender = availableAnswers.map((answer)=>{
+            return(
+                    <button
+                        className={choosenAnswers[index] === answer? 'choosen': null}
+                        onClick={()=>handleAnswerClick(index,answer)}>
+                        {answer}
+                    </button>          
+            )
+        })
         return(
-            <div className="quiz-container" key={index}>
-                    <h1>{result.question}</h1>
-                    <div className="buttons-container">
-                        <button>{result.allAnswers[0]}</button>
-                        <button>{result.allAnswers[1]}</button>
-                        <button>{result.allAnswers[2]}</button>
-                        <button>{result.allAnswers[3]}</button>
-                    </div>
+            <div className="quiz-container" >
+                <h1>{result.question}</h1>
+                <div className="buttons-container">
+
+                   {buttonRender}
+
+                </div>
             </div>
             )
         })
     }
-
     return(
         <>
             <main className="page2-container">
